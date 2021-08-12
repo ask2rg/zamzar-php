2942a46ad30da48d91dbbff5db91ef16befab8a9
ask2rg <88713855+ask2rg@users.noreply.github.com>
2021-08-12 10:43:06 +0530

    test for wamp
    issue to download output file locally

--- zamzar-sample.php
+++ zamzar-sample.php
@@ -1,10 +1,11 @@
 <?php
+
 // Initialise variables
 $endpoint = "https://api.zamzar.com/v1";
-$apiKey = "YOUR_API_KEY";
-$sourceFilePath = "/tmp/test.pdf";
-$convertedFilePath = "/tmp/";
-$targetFormat = "png";
+$apiKey = "__________________________";
+$sourceFilePath = "sample.pdf"; // change your source file
+$convertedFilePath = "/output/"; // change your output folder
+$targetFormat = "txt";
 
 // Start the job
 echo "Starting new conversion job\n";
@@ -23,14 +24,14 @@ echo "Conversion done!\n";
 
 // Download the converted files
 foreach($job->target_files as $file) {
-    echo "Downloading converted file: $file->name\n";
+    echo "Downloading start of converted file: $file->name<br/><br/>";
     download_file($apiKey, $endpoint, $file->id, $convertedFilePath.$file->name);
 }
 
 function start_conversion($apiKey, $endpoint, $sourceFilePath, $targetFormat) {
 
   // Setup variables for cURL
-  $sourceFile = curl_file_create($sourceFilePath);
+  $sourceFile = curl_file_create(realpath($sourceFilePath));
   $postData = array("source_file" => $sourceFile, "target_format" => $targetFormat);
  
   // Execute cURL
@@ -40,10 +41,14 @@ function start_conversion($apiKey, $endpoint, $sourceFilePath, $targetFormat) {
   curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Return response as a string
   curl_setopt($ch, CURLOPT_USERPWD, $apiKey . ":"); // Set the API key as the basic auth username
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
   $body = curl_exec($ch);
   curl_close($ch);
 
   $job = json_decode($body);
+   echo "<pre>";
+print_r($job);
+echo "</pre>";
   $jobID = $job->id;
   return $jobID;
 }
@@ -53,11 +58,15 @@ function check_status($apiKey, $endpoint, $jobID) {
   curl_setopt($ch, CURLOPT_URL, "$endpoint/jobs/$jobID"); // API endpoint
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Return response as a string
   curl_setopt($ch, CURLOPT_USERPWD, $apiKey . ":"); // Set the API key as the basic auth username
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
   $body = curl_exec($ch);
   curl_close($ch);
 
   // Return job query result as JSON
   $job = json_decode($body);
+   echo "<pre>";
+print_r($job);
+echo "</pre>";
   return $job;
 }
 
@@ -66,8 +75,12 @@ function download_file($apiKey, $endpoint, $fileID, $fileSavePath) {
   curl_setopt($ch, CURLOPT_URL, "$endpoint/files/$fileID/content"); // API endpoint
   curl_setopt($ch, CURLOPT_USERPWD, $apiKey . ":"); // Set the API key as the basic auth username
   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
-  $fh = fopen($fileSavePath, "wb");
-  curl_setopt($ch, CURLOPT_FILE, $fh);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  //$fh = fopen($fileSavePath, "wb"); // Error occurs
+  //curl_setopt($ch, CURLOPT_FILE, $fh); // Error occurs
+  echo 'Text output: <br/>';
   $body = curl_exec($ch);
   curl_close($ch);
+
+  echo '<br/><br/> print: <br/><br/>'.print_r($body);
 }
